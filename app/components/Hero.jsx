544ca1964f0110bb1eea71c09930e1c3fb187d4b
kerloshany3 <kerlostablet@gmail.com>
2024-12-06 18:51:50 +0200

    logo

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -3,12 +3,15 @@ import React from 'react'
 
 const Hero = () => {
   return (
-      <div>
+      <div className=' flex'>
           <div>
+              
+              <h3>Empowering Dreams Through Education and Opportunity
+              </h3>
               
           </div>
           <div>
-              <Image className=' bg-primary2 rounded-xl' src='/image.png' width={100} height={500} alt='logo'></Image>
+              <Image className=' bg-primary2 rounded-xl' src='/image.png' width={300} height={500} alt='logo'></Image>
           </div>
     </div>
   )
--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
     <div>
 
       <Header></Header>
-      <Hero></Hero>
+     
     </div>
     
   );
